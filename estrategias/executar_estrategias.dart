aa59472c3b4d6d54d08816fefe692fd78bede812
Rian Alves <rianalvesleal@gmail.com>
2025-06-17 00:38:09 -0300

    Adicionado a função main mais o diretório que terá as estratégias, juntamente com a função de geração de lista aleatória e a função que chama outras funções

---
+++ estrategias/executar_estrategias.dart
@@ -0,0 +1,15 @@
+// Função principal que executa qualquer estratégia sobre a lista
+import 'gerar_lista.dart';
+
+// -----------AVISO--------------- //
+// Necessária a exportação do arquivo que fará a estratégia para a função principal
+// Pois o nome da função passada por parâmetro deve estar listada nas funções declaradas
+// export 'arquivo.dart';
+
+// Ela passa por parâmetro uma variável do tipo função com o seu tipo de retorno e parâmetro
+void executarEstrategia(void Function(List<int>) estrategia) {
+  List<int> lista = gerarLista();
+  print("Lista aleatória gerada:");
+  print(lista);
+  estrategia(lista); // Aplica a estratégia
+}
---
+++ main.dart
@@ -0,0 +1,6 @@
+// Função principal usada somente para fazer a chamada das funções 
+import 'estrategias/executar_estrategias.dart'; 
+
+void main(){
+    
+}
