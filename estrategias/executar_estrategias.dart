9cd454779cb3e9b65cc30377bcb76e4637ea39a2
Isaias Vitor Silva Oliveira <93414564+isaias-vitor@users.noreply.github.com>
2025-06-17 22:36:32 -0300

    Update executar_estrategias.dart
    Adicionada importação e exportação de ordenacao_crescente.dart

--- estrategias/executar_estrategias.dart
+++ estrategias/executar_estrategias.dart
@@ -1,12 +1,14 @@
 // Função principal que executa qualquer estratégia sobre a lista
 import 'gerar_lista.dart';
 import 'ordenar_decrescente.dart';
+import 'ordenacao_crescente.dart';
 
 // -----------AVISO--------------- //
 // Necessária a exportação do arquivo que fará a estratégia para a função principal
 // Pois o nome da função passada por parâmetro deve estar listada nas funções declaradas
 // export 'arquivo.dart';
 export 'ordenar_decrescente.dart';
+export 'ordenacao_crescente.dart';
 
 // Ela passa por parâmetro uma variável do tipo função com o seu tipo de retorno e parâmetro
 void executarEstrategia(void Function(List<int>) estrategia) {
